0b98b0fc2904fdc2f91ba6370b0095876fd2766f
kicqgit config --global user.email <kirill.chistyakov2017@gmail.com>
2022-04-09 10:18:04 +0300

    fix: change confs and package

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// файл vue.config.js должен быть расположен в корневом каталоге проекта
+
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/ambient-music-app/' : '/',
+}
