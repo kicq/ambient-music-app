9327193579c41e9337f918d69f52173b69af32ca
kicq <kirill.chistyakov2017@gmail.com>
2022-05-26 22:08:02 +0300

    deploy

---
+++ precache-manifest.16d532b162847a60f36ad03c1ebfa19d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903bd32f324adcdfceb4",
+    "url": "/ambient-music-app/css/app.96f78684.css"
+  },
+  {
+    "revision": "47fb15ce6eb8bea622e648492898e1bb",
+    "url": "/ambient-music-app/img/arrow.47fb15ce.svg"
+  },
+  {
+    "revision": "89bdca580ab4500174d83499514f8edd",
+    "url": "/ambient-music-app/img/library.89bdca58.svg"
+  },
+  {
+    "revision": "385488db611dd8025c8ac2fb0c58a788",
+    "url": "/ambient-music-app/img/play.385488db.svg"
+  },
+  {
+    "revision": "a580e2e778ba378e0dd688190325197a",
+    "url": "/ambient-music-app/img/play2.a580e2e7.svg"
+  },
+  {
+    "revision": "6834b9cbc467db6e242e254fe7d85b76",
+    "url": "/ambient-music-app/img/settings.6834b9cb.svg"
+  },
+  {
+    "revision": "b72dc3ec210311e5468bf46e88d5fd1f",
+    "url": "/ambient-music-app/img/stop.b72dc3ec.svg"
+  },
+  {
+    "revision": "ae9e51e27e4c6dbcfeb6f9a5866e959c",
+    "url": "/ambient-music-app/img/voice.ae9e51e2.svg"
+  },
+  {
+    "revision": "9a69ef0a91067b122c0164e30e9ffc22",
+    "url": "/ambient-music-app/index.html"
+  },
+  {
+    "revision": "863374706c7337480fc7",
+    "url": "/ambient-music-app/js/about.e8e4ba68.js"
+  },
+  {
+    "revision": "903bd32f324adcdfceb4",
+    "url": "/ambient-music-app/js/app.abfae7b5.js"
+  },
+  {
+    "revision": "d534bd023e8253de5edf",
+    "url": "/ambient-music-app/js/chunk-vendors.9f805c56.js"
+  },
+  {
+    "revision": "38c1b3c75ccc945102c591aaf2127f9a",
+    "url": "/ambient-music-app/manifest.json"
+  },
+  {
+    "revision": "96fec3733ef6970c6625c9b3eed9c9a1",
+    "url": "/ambient-music-app/media/cargo.96fec373.mp3"
+  },
+  {
+    "revision": "3265b76a84d709bc980709ff4ac483db",
+    "url": "/ambient-music-app/media/cargo2.3265b76a.mp3"
+  },
+  {
+    "revision": "4a22b7d185973b24304371f95235b88c",
+    "url": "/ambient-music-app/media/crickets.4a22b7d1.mp3"
+  },
+  {
+    "revision": "e8593461ba20fa832904b8c547655dff",
+    "url": "/ambient-music-app/media/cuckoo.e8593461.mp3"
+  },
+  {
+    "revision": "2925cc1051b10c48e8452402d893e2a8",
+    "url": "/ambient-music-app/media/desertWind.2925cc10.mp3"
+  },
+  {
+    "revision": "46d1bdc79dc9d8a95c2819a74052c9f2",
+    "url": "/ambient-music-app/media/doors.46d1bdc7.mp3"
+  },
+  {
+    "revision": "cc78e6030f331e1012a439ccf8f73dc1",
+    "url": "/ambient-music-app/media/forest_1.cc78e603.mp3"
+  },
+  {
+    "revision": "9e622f0103c49f247d07e5ccbcb405ba",
+    "url": "/ambient-music-app/media/forest_wind.9e622f01.mp3"
+  },
+  {
+    "revision": "3238a1b793182540ced02e3c16086ce2",
+    "url": "/ambient-music-app/media/interface.3238a1b7.mp3"
+  },
+  {
+    "revision": "031ae3f711495816e4c2a110d0da681f",
+    "url": "/ambient-music-app/media/interface2.031ae3f7.mp3"
+  },
+  {
+    "revision": "d24f253d11a9c7c37a9623659fc84dc0",
+    "url": "/ambient-music-app/media/marketplace.d24f253d.mp3"
+  },
+  {
+    "revision": "a1ad12feb3d459750c2d60f60afc1a8d",
+    "url": "/ambient-music-app/media/planetary.a1ad12fe.mp3"
+  },
+  {
+    "revision": "552bdabfc0c3e46e7c03d212292fd4fa",
+    "url": "/ambient-music-app/media/spaceShip.552bdabf.mp3"
+  },
+  {
+    "revision": "f3ed8de47187aa37cd4f22daf111b67c",
+    "url": "/ambient-music-app/media/spaceShipFlight.f3ed8de4.mp3"
+  },
+  {
+    "revision": "21e65b40f070251fea5748b0b02871c1",
+    "url": "/ambient-music-app/media/starwars.21e65b40.mp3"
+  },
+  {
+    "revision": "195ad69e302e475fa7ab6446470b672e",
+    "url": "/ambient-music-app/media/starwarsWeGoTogether.195ad69e.mp3"
+  },
+  {
+    "revision": "2df6fdfb2bbd0e29a4a2902f8dddefaf",
+    "url": "/ambient-music-app/media/tech.2df6fdfb.mp3"
+  },
+  {
+    "revision": "c214fb9eecf73a92a46fbf5645c1a12f",
+    "url": "/ambient-music-app/media/train.c214fb9e.wav"
+  },
+  {
+    "revision": "b1ca0414c9331069e4e07924ae8b03a7",
+    "url": "/ambient-music-app/media/waterfall.b1ca0414.mp3"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ambient-music-app/robots.txt"
+  },
+  {
+    "revision": "e5e8a25de7afdbc7f6d5911b54e5ae61",
+    "url": "/ambient-music-app/sitemap.xml"
+  }
+]);
